c25c39734a5aae14ff4c7adea9a8302cb6632054
Cyuan <j446214@gmail.com>
2026-09-10 18:10:32 +0800

    change convex expectAuth to false

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { ReactNode } from 'react';
-import { ConvexReactClient } from 'convex/react';
-import { authClient } from '@/lib/auth-client';
-import { ConvexBetterAuthProvider } from '@convex-dev/better-auth/react';
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { authClient } from "@/lib/auth-client";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
   // 可選的：當使用者登入才能獲得資料
-  expectAuth: true,
+  expectAuth: false,
 });
 
 export function ConvexClientProvider({
@@ -18,7 +18,11 @@ export function ConvexClientProvider({
   initialToken?: string | null;
 }) {
   return (
-    <ConvexBetterAuthProvider client={convex} authClient={authClient} initialToken={initialToken}>
+    <ConvexBetterAuthProvider
+      client={convex}
+      authClient={authClient}
+      initialToken={initialToken}
+    >
       {children}
     </ConvexBetterAuthProvider>
   );
